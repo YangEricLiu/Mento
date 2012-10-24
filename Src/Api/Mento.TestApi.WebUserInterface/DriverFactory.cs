cc67382e494351abfa3e6315033dac498180923e
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-10-24 05:43:51 +0000

    DriverFactory
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C3774

---
+++ Src/Api/Mento.TestApi.WebUserInterface/DriverFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mento.TestApi.WebUserInterface
+{
+    public class DriverFactory
+    {
+        public static IWebDriver GetDriver(string browserType)
+        {
+            switch (browserType)
+            {
+                case "IE":
+                    return new InternetExplorerDriver();
+                case "Chrome":
+                    return new ChromeDriver();
+                case "FireFox":
+                    return new FirefoxDriver();
+                default:
+                    return null;
+            }
+        }
+    }
+}

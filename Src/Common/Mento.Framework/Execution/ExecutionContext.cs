32347e266905682b0d3dbf8de4e0236b5ff91c8d
Feng ZHANG <feng-aries.zhang@schneider-electric.com>
2012-10-26 03:20:33 +0000

    added execution context class
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C3855

---
+++ Src/Common/Mento.Framework/Execution/ExecutionContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mento.Framework.Execution
+{
+    public static class ExecutionContext
+    {
+        public static Browser Browser { get; set; }
+        public static Language Language { get; set; }
+        public static string Url { get; set; }
+    }
+}

be85c34c036c76f60284ffba3113e5dd40dc0d5c
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-10-26 08:52:21 +0000

    Dictory
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C3879

---
+++ Src/Api/Mento.TestApi.WebUserInterface/DictDataLoad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mento.Utility;
+using Mento.Framework.Execution;
+
+namespace Mento.TestApi.WebUserInterface
+{
+    public static class DictDataLoad
+    {
+        public static Dictionary<string, TypeValue> dictElement = GetLanguageResource.GetLangResource(ExecutionContext.Language);
+        public static Dictionary<string, TypeValue> dictManualElement = XMLHelper.GetManualElementValue();
+    }
+}

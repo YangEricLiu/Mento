53b9fa359a77b792c62f6499cfd117467e270401
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-10-26 08:52:38 +0000

    language
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C3880

---
+++ Src/Api/Mento.TestApi.WebUserInterface/GetLanguageResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mento.Framework.Execution;
+using Mento.Framework;
+using Mento.Utility;
+using Mento.Framework.Constants;
+
+namespace Mento.TestApi.WebUserInterface
+{
+    public class GetLanguageResource
+    {
+        public static Dictionary<string, TypeValue> GetLangResource(Language lang)
+        { 
+            switch (lang)
+            {
+                case Language.CN: return XMLHelper.GetElementMapValue(ConfigurationKey.CN_PATH);
+                case Language.EN: return XMLHelper.GetElementMapValue(ConfigurationKey.EN_PATH);
+                default: return null; 
+            }
+            
+        }
+    }
+}

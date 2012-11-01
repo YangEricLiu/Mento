8627085fdc0aee3dc425cb77f69eb973632f7e29
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-01 06:15:20 +0000

    Add button and toggle button control
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4010

---
+++ Src/Api/Mento.TestApi.WebUserInterface/Controls/Button.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mento.TestApi.WebUserInterface
+{
+    public class Button : JazzControlBase
+    {
+        public void ClickButton(Locator buttonLocator)
+        {
+            ElementLocator.FindElement(buttonLocator).Click();
+        }
+
+        public void FloatOnButton(Locator buttonLocator)
+        {
+            ElementLocator.FloatOn(ElementLocator.FindElement(buttonLocator));
+        }
+    }
+}
---
+++ Src/Api/Mento.TestApi.WebUserInterface/Controls/ToggleButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mento.TestApi.WebUserInterface
+{
+    public class ToggleButton : JazzControlBase
+    {
+        public Boolean IsButtonPressed(Locator buttonLocator)
+        {
+            return true;
+        }
+    }
+}

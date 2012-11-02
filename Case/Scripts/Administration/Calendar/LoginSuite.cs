b999e8798c74dde6192a8aa88156f75179c46439
Feng ZHANG <feng-aries.zhang@schneider-electric.com>
2012-11-02 08:57:47 +0000

    navigator
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4034

--- Case/Scripts/Administration/Calendar/LoginSuite.cs
+++ Case/Scripts/Administration/Calendar/LoginSuite.cs
@@ -8,6 +8,7 @@ using Threading = System.Threading;
 using System;
 using Mento.ScriptCommon.TestData.ClientAccess;
 using Mento.ScriptCommon.Library.Functions;
+using Mento.TestApi.WebUserInterface.Controls;
 
 namespace Mento.Script.Administration.Calendar
 {
@@ -17,6 +18,18 @@ namespace Mento.Script.Administration.Calendar
     [Owner("Aries")]
     public class LoginSuite : TestSuiteBase
     {
+        [SetUp]
+        public void SetUp()
+        {
+            ElementLocator.OpenJazz();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //ElementLocator.QuitJazz();
+        }
+
         [Test]
         [CaseID("TA-Trial-Login-001")]
         [MultipleTestDataSource(typeof(LoginData[]), typeof(LoginSuite), "TA-Trial-Login-001")]
@@ -24,15 +37,26 @@ namespace Mento.Script.Administration.Calendar
         {
             //throw new Exception(loginData.ToString());
 
-            ElementLocator.OpenJazz();
 
             //Threading.Thread.Sleep(10000);
 
             FunctionWrapper.Login.Login(loginData.InputData);
             //Assert.IsTrue();
 
-            ElementLocator.QuitJazz();
 
+        }
+
+        [Test]
+        [CaseID("TA-Trial-Login-002")]
+        public void LoginJazzAndNavigage()
+        {
+            FunctionWrapper.Login.Login();
+
+            ElementLocator.WaitForElement(new Locator("header-btn-homepage-btnEl", ByType.ID), 600);
+
+            var navigator = ControlAccess.GetControl<Navigator>();
+
+            navigator.NavigateToTarget(NavigationTarget.TagSettings);
         }
     }
 }
---
+++ Src/Api/Mento.TestApi.WebUserInterface/Controls/Navigator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mento.TestApi.WebUserInterface.Controls
+{
+    public class Navigator : JazzControlBase
+    {
+        public void NavigateToTarget(NavigationTarget target)
+        {
+            Locator[] pathLocators = this.GetParentPathLocators(target);
+
+            for (var i = 0; i < pathLocators.Length; i++)
+            {
+                var element = ElementLocator.FindElement(pathLocators[i]);
+
+                element.Click();
+
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+
+        private Locator[] GetParentPathLocators(NavigationTarget target)
+        {
+            List<Locator> pathLocators = new List<Locator>();
+
+            NavigationTarget? currentTarget = target;
+
+            while (currentTarget.HasValue)
+            {
+                var targetItem = NavigatorItem.NavigationItems[currentTarget.Value];
+
+                pathLocators.Add(targetItem.Locator);
+
+                currentTarget = targetItem.Parent;
+            }
+
+            pathLocators.Reverse();
+
+            return pathLocators.ToArray();
+        }
+    }
+
+    internal class NavigatorItem
+    {
+        public static Dictionary<NavigationTarget, NavigatorItem> NavigationItems = new Dictionary<NavigationTarget, NavigatorItem>()
+        {
+            {NavigationTarget.HomePage,new NavigatorItem( NavigationTarget.HomePage, null, "header-btn-homepage-btnEl",ByType.ID)},
+            {NavigationTarget.EnergyView, new NavigatorItem(NavigationTarget.EnergyView, null,"header-btn-energyservice-btnEl",ByType.ID)},
+            {NavigationTarget.Settings, new NavigatorItem(NavigationTarget.Settings,null,"header-btn-setting-btnEl",ByType.ID)},
+
+            {NavigationTarget.PlatformSettings, new NavigatorItem(NavigationTarget.PlatformSettings,NavigationTarget.Settings,"setting-tab-platformsetting-btn-btnEl",ByType.ID)},
+            {NavigationTarget.TagSettings, new NavigatorItem(NavigationTarget.TagSettings,NavigationTarget.Settings,"setting-tab-tagmrg-btn-btnEl",ByType.ID)},
+            {NavigationTarget.HierarchySettings, new NavigatorItem(NavigationTarget.HierarchySettings,NavigationTarget.Settings,"setting-tab-hiersetting-btn-btnEl",ByType.ID)},
+            {NavigationTarget.AssociationSettings, new NavigatorItem(NavigationTarget.AssociationSettings,NavigationTarget.Settings,"setting-tab-tagassoc-btn-btnEl",ByType.ID)},
+        };
+
+        public NavigationTarget? Parent
+        {
+            get;
+            set;
+        }
+
+        public NavigationTarget Target
+        {
+            get;
+            set;
+        }
+
+        public Locator Locator
+        {
+            get;
+            set;
+        }
+
+        public NavigatorItem(NavigationTarget self, NavigationTarget? parent, string locatorValue, ByType locatorType)
+        {
+            this.Target = self;
+            this.Parent = parent;
+            this.Locator = new Locator(locatorValue, locatorType);
+        }
+    }
+
+    public enum NavigationTarget
+    {
+        HomePage = 1, //header-btn-homepage-btnEl
+        EnergyView = 2, //header-btn-energyservice-btnEl
+        Settings = 3,//header-btn-setting-btnEl
+
+        PlatformSettings = 4, //setting-tab-platformsetting-btn-btnEl
+        TagSettings = 5, //setting-tab-tagmrg-btn-btnEl
+        HierarchySettings = 6, //setting-tab-hiersetting-btn-btnEl
+        AssociationSettings = 7 //setting-tab-tagassoc-btn-btnEl
+    }
+}

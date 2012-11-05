ffadb9977d4a770dc57026dcb29f9af2152c6140
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-05 09:26:13 +0000

    add comments
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4071

--- Src/Api/Mento.TestApi.WebUserInterface/Controls/Button.cs
+++ Src/Api/Mento.TestApi.WebUserInterface/Controls/Button.cs
@@ -7,14 +7,34 @@ namespace Mento.TestApi.WebUserInterface
 {
     public class Button : JazzControlBase
     {
+        /// <summary>
+        /// Simulate the mouse click the button
+        /// </summary>
+        /// <param name="buttonLocator"></param>
+        /// <returns></returns>
         public void ClickButton(Locator buttonLocator)
         {
             ElementLocator.FindElement(buttonLocator).Click();
         }
 
+        /// <summary>
+        /// Simulate the mouse float the button
+        /// </summary>
+        /// <param name="buttonLocator"></param>
+        /// <returns></returns>
         public void FloatOnButton(Locator buttonLocator)
         {
             ElementLocator.FloatOn(ElementLocator.FindElement(buttonLocator));
+        }
+
+        /// <summary>
+        /// Get the text value of the button
+        /// </summary>
+        /// <param name="buttonLocator"></param>
+        /// <returns>the text value of this button</returns>
+        public string GetButtonValue(Locator buttonLocator)
+        {
+            return ElementLocator.FindElement(buttonLocator).Text;
         }
     }
 }

2f495986b8bb6769f86ba99164c67c5b33ee00ef
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-05 08:41:05 +0000

    add comments
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4066

--- Src/Api/Mento.TestApi.WebUserInterface/Controls/Grid.cs
+++ Src/Api/Mento.TestApi.WebUserInterface/Controls/Grid.cs
@@ -9,16 +9,26 @@ namespace Mento.TestApi.WebUserInterface
 {
     public class Grid : JazzControlBase
     {
+        /// <summary>
+        /// Simulate mouse focus on the row which "Name" is "cellName"
+        /// </summary>
+        /// <param name="cellName"></param>
+        /// <returns></returns>
         public void FocusOnRow(string cellName)
         {
-            var locator = this.GetVariableLocator(ElementKey.IsTreeNodeExpand, ManualElementName.cellName, cellName);
+            var locator = this.GetVariableLocator(ElementKey.RowCellName, ManualElementName.cellName, cellName);
 
             ElementLocator.FindElement(locator).Click();
         }
 
+        /// <summary>
+        /// Simulate mouse float on the row which "Name" is "cellName"
+        /// </summary>
+        /// <param name="cellName"></param>
+        /// <returns></returns>
         public void FloatOnRow(string cellName)
         {
-            var locator = this.GetVariableLocator(ElementKey.IsTreeNodeExpand, ManualElementName.cellName, cellName);
+            var locator = this.GetVariableLocator(ElementKey.RowCellName, ManualElementName.cellName, cellName);
 
             ElementLocator.FloatOn(ElementLocator.FindElement(locator));
         }

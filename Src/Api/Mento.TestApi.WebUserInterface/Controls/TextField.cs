6cd5061334cabcd42df103c0736e593065536a9d
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-05 08:35:30 +0000

    add comments
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4065

--- Src/Api/Mento.TestApi.WebUserInterface/Controls/TextField.cs
+++ Src/Api/Mento.TestApi.WebUserInterface/Controls/TextField.cs
@@ -8,29 +8,41 @@ namespace Mento.TestApi.WebUserInterface
 {
     public class TextField : JazzControlBase
     {
+
+        /// <summary>
+        /// Fill the text to object text field, clear the field first, then input text
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
         public void FillIn(string obj, string content)
         {
-            //string textLocator = DictDataLoad.dictElement[obj].Value;
-            //ByType type = DictDataLoad.dictElement[obj].Type;
             var locator = ElementDictionary[obj];
 
             ElementLocator.FindElement(locator).Clear();
             ElementLocator.FindElement(locator).SendKeys(content);
         }
 
+        /// <summary>
+        /// Append the text to object text field, not clear the field first, input text to the end
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
         public void Append(string obj, string content)
         {
-            //string textLocator = DictDataLoad.dictElement[obj].Value;
-            //ByType type = DictDataLoad.dictElement[obj].Type;
             var locator = ElementDictionary[obj];
 
             ElementLocator.FindElement(locator).SendKeys(content);
         }
 
+        /// <summary>
+        /// Get the text field value
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>the text value in the text field</returns>
         public string GetValue(string obj)
         {
-            //string textLocator = DictDataLoad.dictElement[obj].Value;
-            //ByType type = DictDataLoad.dictElement[obj].Type;
             var locator = ElementDictionary[obj];
 
             return ElementLocator.FindElement(locator).GetAttribute("value");

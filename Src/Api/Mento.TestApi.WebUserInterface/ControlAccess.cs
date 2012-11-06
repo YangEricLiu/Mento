f019c2fc87678447939eb6fcbf1083d087ace817
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-06 04:25:08 +0000

    add class comments
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4088

--- Src/Api/Mento.TestApi.WebUserInterface/ControlAccess.cs
+++ Src/Api/Mento.TestApi.WebUserInterface/ControlAccess.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace Mento.TestApi.WebUserInterface
 {
+    /// <summary>
+    /// Get the instance of the special control.
+    /// </summary>
     public class ControlAccess
     {
+        /// <summary>
+        /// Get the instance of the special control.
+        /// </summary>
+        /// <returns>The instance of one special control</returns>
         public static T GetControl<T>() where T : JazzControlBase, new()
         {
             return new T();

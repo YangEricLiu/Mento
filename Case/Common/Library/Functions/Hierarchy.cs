0723644e80e43e356f3ff9871cc5c16a6d809db6
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-06 07:49:04 +0000

    add some method
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4102

--- Case/Common/Library/Functions/Hierarchy.cs
+++ Case/Common/Library/Functions/Hierarchy.cs
@@ -9,6 +9,9 @@ using Mento.ScriptCommon.TestData.Administration.Hierarchy.HierarchyManagement;
 
 namespace Mento.ScriptCommon.Library.Functions
 {
+    /// <summary>
+    /// The business logic implement of hierarchy setting
+    /// </summary>
     public class Hierarchy
     {
         private static Dictionary<string, Locator> ElementDictionary = ResourceManager.GetElementDictionary();
@@ -16,11 +19,15 @@ namespace Mento.ScriptCommon.Library.Functions
         private TreeView treeViewInstance = ControlAccess.GetControl<TreeView>();
         private TextField textFieldInstance = ControlAccess.GetControl<TextField>();
         private ComboBox comboBoxInstance = ControlAccess.GetControl<ComboBox>();
+        private Navigator navigatorInstance = ControlAccess.GetControl<Navigator>();
 
-        private void PrepareToAddNode(string treeNodeName)
+        /// <summary>
+        /// Click one hierarchy node then click "add hierarchy" button
+        /// </summary>
+        /// <param name="treeNodeName">Hierarchy node name</param>
+        /// <returns></returns>
+        public void PrepareToAddNode(string treeNodeName)
         {
-            //string addHierarchyButton = DictDataLoad.dictElement[ElementKey.AddHierarchyButton].value;
-            //ByType type = DictDataLoad.dictElement[ElementKey.AddHierarchyButton].type;
             var locator = ElementDictionary[ElementKey.AddHierarchyButton];
 
             treeViewInstance.FocusOnTreeNode(treeNodeName);
@@ -28,24 +35,58 @@ namespace Mento.ScriptCommon.Library.Functions
             ElementLocator.FindElement(locator).Click();
         }
 
+        /// <summary>
+        /// Click one hierarchy node
+        /// </summary>
+        /// <param name="treeNodeName">Hierarchy node name</param>
+        /// <returns></returns>
+        public void FocusOnHierarchyNode(string treeNodeName)
+        {
+            treeViewInstance.FocusOnTreeNode(treeNodeName);
+        }
+
+        /// <summary>
+        /// Navigate to hierarchy setting
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        public void NavigatorToHSetting()
+        {
+            navigatorInstance.NavigateToTarget(NavigationTarget.Settings);
+            ElementLocator.Pause(500);
+            navigatorInstance.NavigateToTarget(NavigationTarget.HierarchySettings);
+        }
+
+        /// <summary>
+        /// Click save button to add new hierarchy node
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
         public void ClickSaveButton()
         {
-            //string buttonLocator = DictDataLoad.dictElement[ElementKey.SaveButton].value;
-            //ByType type = DictDataLoad.dictElement[ElementKey.SaveButton].type;
             var locator = ElementDictionary[ElementKey.SaveButton];
 
             ElementLocator.FindElement(locator).Click();
         }
 
+        /// <summary>
+        /// Click cancel button to cancel add new hierarchy node
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
         public void ClickCancelButton()
         {
-            //string buttonLocator = DictDataLoad.dictElement[ElementKey.CancelButton].value;
-            //ByType type = DictDataLoad.dictElement[ElementKey.CancelButton].type;
             var locator = ElementDictionary[ElementKey.CancelButton];
 
             ElementLocator.FindElement(locator).Click();
         }
-        
+
+        /// <summary>
+        /// Input name, code type and comments of the new hierarchy node 
+        /// </summary>
+        /// <param name="treeNodeName">Hierarchy node name</param>
+        /// <param name="input">Test data</param>
+        /// <returns></returns>
         public void AddHierarchyNode(string treeNodeName, HierarchyInputData input)
         {
             PrepareToAddNode(treeNodeName);
@@ -53,33 +94,90 @@ namespace Mento.ScriptCommon.Library.Functions
             textFieldInstance.FillIn(ElementKey.HierarchyName, input.Name);
             textFieldInstance.FillIn(ElementKey.HierarchyCode, input.Code);
             comboBoxInstance.DisplayItems(ElementKey.HierarchyType);
-            comboBoxInstance.SelectItem(ElementKey.Orgnization);
+            comboBoxInstance.SelectItem(input.Type);
             textFieldInstance.FillIn(ElementKey.HierarchyComment, input.Comment);
-
-            ClickSaveButton();
         }
 
+        /// <summary>
+        /// After click save button, waiting for add successful message box pop up
+        /// </summary>
+        /// <param name="timeout">Waiting time</param>
+        /// <returns></returns>
         public void WaitForCreateOKDisplay(int timeout)
         { 
-            //string CreateOK = DictDataLoad.dictElement[ElementKey.CreateOKText].value;
-            //ByType type = DictDataLoad.dictElement[ElementKey.CreateOKText].type;
             var locator = ElementDictionary[ElementKey.CreateOKText];
 
             ElementLocator.WaitForElement(locator, timeout);
         }
 
+        /// <summary>
+        /// Confirm the add successful popup message box
+        /// </summary>
+        /// <returns></returns>
         public void ConfirmCreateOKMagBox()
         {
-            //string OKButton = DictDataLoad.dictElement[ElementKey.OKButton].value;
-            //ByType type = DictDataLoad.dictElement[ElementKey.OKButton].type;
             var locator = ElementDictionary[ElementKey.OKButton];
 
             ElementLocator.FindElement(locator).Click();
         }
 
+        /// <summary>
+        /// Judge whether the nodes are Child-Parent
+        /// </summary>
+        /// <param name="nodeChild">Child node name</param>
+        /// <param name="nodeParent">Parent node name</param>
+        /// <returns>True if the nodes are Child-Parent, false if not</returns>
         public Boolean IsNodesChildParent(string nodeChild, string nodeParent)
         {
             return treeViewInstance.IsNodesParentChild(nodeChild, nodeParent);
+        }
+
+        /// <summary>
+        /// Judge whether the hierarchy is present
+        /// </summary>
+        /// <param name="treeNodeName">Hierarchy node name</param>
+        /// <returns>True if the node is present, false if not</returns>
+        public Boolean IsTreeNodePresent(string treeNodeName)
+        {
+            return treeViewInstance.IsTreeNodePresent(treeNodeName);
+        }
+
+        /// <summary>
+        /// Collapse the hierarchy  node
+        /// </summary>
+        /// <param name = "treeNodeName">Hierarchy node name which will be collapsed</param>
+        /// <returns></returns>
+        public void CollapseNode(string treeNodeName)
+        {
+            treeViewInstance.Collapse(treeNodeName);
+        }
+
+        /// <summary>
+        /// Expand the hierarchy  node
+        /// </summary>
+        /// <param name = "treeNodeName">Hierarchy node name which will be expanded</param>
+        /// <returns></returns>
+        public void ExpandNode(string treeNodeName)
+        {
+            treeViewInstance.Expand(treeNodeName);
+        }
+
+        /// <summary>
+        /// Expand the hierarchy  node
+        /// </summary>
+        /// <param name = "treeNodeName">Hierarchy node name which will be expanded</param>
+        /// <returns></returns>
+        public Boolean IsNodeValueCorrect(string treeNodeName, HierarchyInputData input)
+        {
+            FocusOnHierarchyNode(treeNodeName);
+
+            textFieldInstance.GetValue(ElementKey.HierarchyName);
+            textFieldInstance.GetValue(ElementKey.HierarchyCode);
+            comboBoxInstance.GetValue(ElementKey.HierarchyType);
+            textFieldInstance.GetValue(ElementKey.HierarchyComment);
+            textFieldInstance.FillIn(ElementKey.HierarchyComment, input.Comment);
+
+            return true;
         }
     }
 }

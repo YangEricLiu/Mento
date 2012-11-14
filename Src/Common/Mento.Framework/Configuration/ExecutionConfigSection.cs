f3d94ea738c9fee5c76de04dfb203beb58b9e6ad
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-14 01:40:23 +0000

    Add config file execution
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4280

---
+++ Src/Common/Mento.Framework/Configuration/ExecutionConfigSection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Mento.Framework.Configuration
+{
+    public class ExportConfigSection : ConfigurationSection
+    {
+
+    }
+}

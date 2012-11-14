004f33620dbdcb9417a479cb69ca62092f93421e
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-14 03:30:14 +0000

    Add execution and export location config section
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4295

---
+++ Src/Common/Mento.Framework/Configuration/ExecutionConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Collections;
+using System.Collections.Specialized;
+using Mento.Framework.Constants;
+
+namespace Mento.Framework.Configuration
+{
+    public static class ExecutionConfig
+    {
+        public static string GetExecutionConfig(string configKey)
+        {
+            var dictNameValueCollection = ConfigurationManager.GetSection(ConfigurationKey.EXECUTION_CONFIGURATION) as NameValueCollection;
+
+            return dictNameValueCollection[configKey];
+        }
+    }
+}
---
+++ Src/Common/Mento.Framework/Configuration/ExportConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Collections;
+using System.Collections.Specialized;
+using Mento.Framework.Constants;
+
+namespace Mento.Framework.Configuration
+{
+    public static class ExportConfig
+    {
+        public static string GetExportLocationConfig(string configKey)
+        {
+            var dictNameValueCollection = ConfigurationManager.GetSection(ConfigurationKey.EXPORT_LOCATION_CONFIGURATION) as NameValueCollection;
+
+            return dictNameValueCollection[configKey];
+        }
+    }
+}

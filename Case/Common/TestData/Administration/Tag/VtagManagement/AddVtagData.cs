94388299484c3e9ecb44c63604f0522df79eaf70
XiaoHong SONG <xiaohong-nancy.song@schneider-electric.com>
2012-11-14 07:37:22 +0000

    fix build error
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4315

---
+++ Case/Common/TestData/Administration/Tag/VtagManagement/AddVtagData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mento.TestApi.TestData;
+using Mento.Utility;
+
+namespace Mento.ScriptCommon.TestData.Administration.Tag.VtagManagement
+{
+    public class AddVtagData : TestDataBase<VtagInputData, ExpectedTestDataBase>
+    {
+    }
+
+    public class VtagInputData : InputTestDataBase
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Commodity { get; set; }
+        public string UOM { get; set; }
+        public string Step { get; set; }
+        public string CalculationType { get; set; }
+        public string Comment { get; set; }
+
+        public VtagInputData(string name, string code, string Commodity, string UOM, string Step, string CalculationType, string Comment)
+        {
+            this.Name = name;
+            this.Code = code;
+            this.Commodity = Commodity;
+            this.UOM = UOM;
+            this.Step = Step;
+            this.CalculationType = CalculationType;
+            this.Comment = Comment;
+
+        }
+    }
+}

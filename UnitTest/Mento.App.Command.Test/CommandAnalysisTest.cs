4a21219af0e0c25d3db10d75c36e19d5cf6feee1
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2012-11-21 08:21:49 +0000

    Add command analysis unit test
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C4526

---
+++ UnitTest/Mento.App.Command.Test/CommandAnalysisTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App.CommandAnalysis;
+
+namespace Mento.App.Command.Test
+{
+    [TestClass]
+    public class CommandAnalysisTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //
+        }
+
+        [TestMethod]
+        public void CommandAnalyerTest()
+        {
+            string[] cmds = new string[] { "result", "view", "-result", "TA-Example-001" };
+
+            CommandAnalyer.CommandsAnalysis(cmds);
+
+            
+            //Assert.AreEqual(AppDomain.CurrentDomain.BaseDirectory.ToString(), "hello");
+        }
+    }
+}

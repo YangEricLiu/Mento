549509c28b3ed6fa45a4fd034814727a1571bea1
sesa270128 <sesa270128@apa.tfs.local>
2013-05-09 07:41:28 +0000

    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C6793

---
+++ Case/SmokeTest_Eric/Scripts/SmokeTest/Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mento.Framework.Script;
+using NUnit.Framework;
+using Mento.Framework.Attributes;
+using Mento.ScriptCommon.Library;
+
+namespace Mento.Script.SmokeTest
+{
+    [TestFixture]
+    [Owner("Eric")]
+    [CreateTime("2013-05-09")]
+    [ManualCaseID("TC-J1-SmokeTest-031")]
+    public class Login : TestSuiteBase
+    {
+        [Test]
+        [CaseID("TC-J1-SmokeTest-031")]
+        public void LoginAsPlatformAdmin()
+        {
+            JazzFunction.LoginPage.LoginToAdmin();
+
+            Assert.IsTrue(JazzFunction.LoginPage.IsAlreadyLogin());
+        }
+    }
+}

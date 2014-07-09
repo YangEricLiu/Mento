009ce70acc709b8b2fc7b50702d7f763a5403efb
DongNa WANG <dongna-emma.wang@schneider-electric.com>
2014-07-09 07:32:13 +0000

    update
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C11389

---
+++ Case/Src/Scripts/EnergyView/EnergyManagement/SaveToDashboardSuite.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Mento.Framework.Attributes;
+using Mento.ScriptCommon.Library;
+using Mento.ScriptCommon.Library.Functions;
+using Mento.TestApi.WebUserInterface;
+using Mento.Framework.Script;
+using Mento.TestApi.WebUserInterface.Controls;
+using Mento.TestApi.WebUserInterface.ControlCollection;
+using Mento.ScriptCommon.TestData.EnergyView;
+using Mento.TestApi.TestData;
+using System.Data;
+using Mento.Utility;
+
+namespace Mento.Script.EnergyView.EnergyManagement
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    [ManualCaseID("TC-J1-FVT-SaveToDashboard-101"), CreateTime("2014-07-09"), Owner("Emma")]
+    public class SaveToDashboardSuite : TestSuiteBase
+    {
+        private static Widget Widget = JazzFunction.Widget;
+        private static HomePage HomePagePanel = JazzFunction.HomePage;
+        private static EnergyAnalysisPanel EnergyAnalysis = JazzFunction.EnergyAnalysisPanel;
+        private static EnergyViewToolbar EnergyViewToolbar = JazzFunction.EnergyViewToolbar;
+        private static MutipleHierarchyCompareWindow MultiHieCompareWindow = JazzFunction.MutipleHierarchyCompareWindow;
+        private static SaveToDashboardDialog SaveToDs = new SaveToDashboardDialog();
+        private static RankPanel CorporateRanking = JazzFunction.RankPanel;
+
+        [SetUp]
+        public void CaseSetUp()
+        {
+            Widget.NavigateToAllDashboard();
+            TimeManager.MediumPause();
+        }
+
+        [TearDown]
+        public void CaseTearDown()
+        {
+            JazzFunction.LoginPage.RefreshJazz("NancyCustomer1");
+            TimeManager.LongPause();
+        }
+
+        [Test]
+        [CaseID("TC-J1-FVT-SaveToDashboard-101-1")]
+        [MultipleTestDataSource(typeof(MaximizeWidgetData[]), typeof(SaveToDashboardSuite), "TC-J1-FVT-SaveToDashboard-101-1")]
+        public void EnergyAnalysisSingleHierarchySaveTDashboard(EnergyViewOptionData input)
+        {
+            HomePagePanel.SelectCustomer("NancyCostCustomer2");
+            TimeManager.MediumPause();
+
+            EnergyAnalysis.NavigateToEnergyAnalysis();
+            TimeManager.MediumPause();
+
+            var dashboard = input.InputData.DashboardInfos;
+
+            //A. Select Single Hierarchy node 楼宇A BuildingA_P1_Electricity.
+            EnergyAnalysis.SelectHierarchy(input.InputData.Hierarchies);
+            JazzMessageBox.LoadingMask.WaitSubMaskLoading();
+            TimeManager.MediumPause();
+
+            //Check tag and view line chart
+            EnergyAnalysis.CheckTag(input.InputData.TagNames[0]);
+            EnergyViewToolbar.ClickViewButton();
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            EnergyAnalysis.Toolbar.SaveToDashboard(dashboard[0].WigetName, dashboard[0].HierarchyName, dashboard[0].IsCreateDashboard, dashboard[0].DashboardName);
+            JazzMessageBox.LoadingMask.WaitLoading();
+            TimeManager.LongPause();
+
+            //Add widget successfully into the dashboard and displayed in the last available space.
+            Widget.NavigateToAllDashboard();
+            TimeManager.LongPause();
+            HomePagePanel.SelectHierarchyNode(dashboard[0].HierarchyName);
+            TimeManager.LongPause();
+
+            HomePagePanel.ClickDashboardButton(dashboard[0].DashboardName);
+            JazzMessageBox.LoadingMask.WaitDashboardHeaderLoading(15);
+            TimeManager.LongPause();
+
+            Assert.AreEqual(12, HomePagePanel.GetWidgetsNumberOfDashboard());
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnDashboard(dashboard[0].WigetName));            
+        }
+
+    }
+}

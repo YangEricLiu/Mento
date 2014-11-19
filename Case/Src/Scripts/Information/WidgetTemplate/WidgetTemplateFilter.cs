64ede8c039ba41e4649f6047fd92ffd07d26fb51
xuemei yu <xuemei-cathy.yu@schneider-electric.com>
2014-11-19 07:54:16 +0000

    cathy
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C12162

--- Case/Src/Scripts/Information/WidgetTemplate/WidgetTemplateFilter.cs
+++ Case/Src/Scripts/Information/WidgetTemplate/WidgetTemplateFilter.cs
@@ -44,7 +44,7 @@ namespace Mento.Script.Information.WidgetTemplate
         }
 
         [Test]
-        [CaseID("TC-J1-FVT-WidgetTemplate-Filter-101")]
+        [CaseID("TC-J1-FVT-WidgetTemplate-Filter-101"), CreateTime("2014-11-19"), Owner("Cathy")]
         [MultipleTestDataSource(typeof(MaximizeWidgetData[]), typeof(WidgetTemplateFilterSuite), "TC-J1-FVT-WidgetTemplate-Filter-101")]
         public void WidgetTemplateFilter101(MaximizeWidgetData input)
         {
@@ -59,14 +59,207 @@ namespace Mento.Script.Information.WidgetTemplate
             TimeManager.LongPause();
             Widget.ClickWidgetTemplateApplyFilterButton();
             Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));
+            TimeManager.LongPause();
             //Click applyfilter button, 行为 still check, 年逐月能耗工休比 still display
             Widget.ClickWidgetTemplateFilterButton();
-            //Widget.IsWidgetTemplateChecked(filtercondition[0].TargetObject[3]);
+            TimeManager.LongPause();
+            //verify 行为 check, other uncheck
             Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));
-            //Widget.UncheckTargetObjectInWidgetTemplateCheckBox(filtercondition[0].TargetObject[3]);
-            //TimeManager.LongPause();
-            //Widget.ClickWidgetTemplateApplyFilterButton();
-            //Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));          
+            TimeManager.LongPause();
+            Assert.IsTrue(Widget.IsWidgetTemplateChecked(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[3]));
+            TimeManager.LongPause();
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[0]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[2]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[3]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[4]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[5]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[6]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[7]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[8]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[9]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[10]));
+
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[11]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[12]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[13]));
+
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[14]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[15]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[16]));
+
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[17]));
+            Assert.IsTrue(Widget.IsWidgetTemplateUnChecked(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[18]));
+
+            //uncheck 行为
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[3]);
+            
+
+            //check 建筑 单位指标
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[3]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[4]));
+            Widget.ClickWidgetTemplateFilterButton();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[3]);
+            
+            //Check 管理，行为，能源消耗，按年
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[3]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[0]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[4]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[5]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[6]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[7]));
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateFilterButton();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[3]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[0]);
+
+            //Check 建筑，按年
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[0]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[1]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[2]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[3]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[4]));
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateFilterButton();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[0]);
+
+            //check 管理，集团排名，能效分析，柱状图
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[6]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[1]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[6]));
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateFilterButton();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[6]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[1]);
+
+            //check 管理，碳排放，成本，柱状图
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[2]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[1]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[1]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[2]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[3]));
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateFilterButton();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[2]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[1]);
+            
+            //check 能效标识图
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[2]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[5]));
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateFilterButton();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[2]);
+
+            //check 时段能耗比，线状图
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[4]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[0]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));
+            TimeManager.LongPause();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[4]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[0]);
+
+            //check all
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[2]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[3]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[2]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[3]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[4]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[5]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[6]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[2]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[1]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[2]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[1]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[1]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[2]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[3]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[4]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[5]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[6]));
+            Assert.IsTrue(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[7]));
+            TimeManager.LongPause();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[2]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[0], input.InputData.TargetObject[3]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[2]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[3]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[4]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[5]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[6]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[2]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[1]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[3], input.InputData.TargetObject[2]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[4], input.InputData.TargetObject[1]);
+
+            //check 能效分析，碳排放
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[0]);
+            Widget.CheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[1]);
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateApplyFilterButton();
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[0]));
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[1]));
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[2]));
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[3]));
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[4]));
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[5]));
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[6]));
+            Assert.IsFalse(HomePagePanel.IsWidgetExistedOnQuickCreateWidget(widgetname[7]));
+            TimeManager.LongPause();
+            Widget.ClickWidgetTemplateFilterButton();
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[1], input.InputData.TargetObject[0]);
+            Widget.UncheckWidgetTemplateCheckBox(input.InputData.FilterPropertiesName[2], input.InputData.TargetObject[1]);
+
+        }
+
         }
     }
-}

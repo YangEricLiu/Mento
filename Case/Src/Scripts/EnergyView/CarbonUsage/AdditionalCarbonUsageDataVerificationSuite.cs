e305954815f8807d99b9d12150f98f59f6ab5ee7
XiaoHong SONG <xiaohong-nancy.song@schneider-electric.com>
2014-11-25 07:49:14 +0000

    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C12218

---
+++ Case/Src/Scripts/EnergyView/CarbonUsage/AdditionalCarbonUsageDataVerificationSuite.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Mento.Framework.Attributes;
+using Mento.ScriptCommon.Library;
+using Mento.ScriptCommon.Library.Functions;
+using Mento.TestApi.WebUserInterface;
+using Mento.Framework.Script;
+using Mento.TestApi.WebUserInterface.Controls;
+using Mento.TestApi.WebUserInterface.ControlCollection;
+using Mento.ScriptCommon.TestData.EnergyView;
+using Mento.TestApi.TestData;
+using System.Data;
+using Mento.Utility;
+
+namespace Mento.Script.EnergyView.CarbonUsage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    [ManualCaseID("TC-J1-FVT-AdditionalCarbonUsageDataVerification-DataView-101"), CreateTime("2013-12-25"), Owner("Cathy")]
+    public class AdditionalCarbonUsageDataVerificationSuite : TestSuiteBase
+    {
+        [SetUp]
+        public void CaseSetUp()
+        {
+            CarbonUsage.NavigateToCarbonUsage();
+            TimeManager.MediumPause();
+        }
+
+        [TearDown]
+        public void CaseTearDown()
+        {
+            JazzFunction.Navigator.NavigateHome();
+
+            HomePagePanel.SelectCustomer("NancyCustomer1");
+            TimeManager.MediumPause();
+            //HomePagePanel.ExitJazz();
+
+            //JazzFunction.LoginPage.LoginWithOption("SchneiderElectricChina", "P@ssw0rdChina", "NancyCustomer1");
+            //TimeManager.MediumPause();
+        }
+
+        private static CarbonUsagePanel CarbonUsage = JazzFunction.CarbonUsagePanel;
+        private static EnergyViewToolbar EnergyViewToolbar = JazzFunction.EnergyViewToolbar;
+        private static HomePage HomePagePanel = JazzFunction.HomePage;
+
+        [Test]
+        [CaseID("TC-J1-FVT-AdditionalCarbonUsageDataVerification-DataView-101-1")]
+        [MultipleTestDataSource(typeof(CarbonUsageData[]), typeof(AdditionalCarbonUsageDataVerificationSuite), "TC-J1-FVT-AdditionalCarbonUsageDataVerification-DataView-101-1")]
+        public void AdditionalCarbonUsageDataVerification01(CarbonUsageData input)
+        {
+            HomePagePanel.SelectCustomer("NancyCustomer1");
+            TimeManager.MediumPause();
+
+            CarbonUsage.NavigateToCarbonUsage();
+            TimeManager.MediumPause();
+
+            //select "NancyCustomer1/GreenieSite/GreenieBuilding"
+            CarbonUsage.SelectHierarchy(input.InputData.Hierarchies);
+            JazzMessageBox.LoadingMask.WaitSubMaskLoading();
+            TimeManager.MediumPause();
+
+            //Time range = 2012-1-1 to 2013-12-18
+            var ManualTimeRange = input.InputData.ManualTimeRange;
+            EnergyViewToolbar.SetDateRange(ManualTimeRange[0].StartDate, ManualTimeRange[0].EndDate);
+            TimeManager.ShortPause();
+
+            EnergyViewToolbar.SelectCarbonConvertTarget(input.InputData.CarbonType);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+           //Select "介质总览"
+            CarbonUsage.SelectCommodity();
+            TimeManager.ShortPause();
+           
+            EnergyViewToolbar.View(EnergyViewType.List);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            //"Year"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Year);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[0], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[0], input.InputData.failedFileName[0]);
+
+            //"Month"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Month);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[1], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[1], input.InputData.failedFileName[1]);
+
+            //Select "介质单项" = "电"+"自来水"+"煤"
+            CarbonUsage.SelectCommodity(input.InputData.commodityNames);
+            TimeManager.ShortPause();
+
+            EnergyViewToolbar.View(EnergyViewType.List);
+            JazzMessageBox.LoadingMask.WaitSubMaskLoading();
+            TimeManager.LongPause();
+
+            //"Year"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Year);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[2], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[2], input.InputData.failedFileName[2]);
+
+            //"Month"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Month);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[3], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[3], input.InputData.failedFileName[3]);
+
+            //Select "电"+"自来水"
+            CarbonUsage.DeSelectCommodity(input.InputData.commodityNames[2]);
+            TimeManager.ShortPause();
+
+            EnergyViewToolbar.View(EnergyViewType.List);
+            JazzMessageBox.LoadingMask.WaitSubMaskLoading();
+            TimeManager.LongPause();
+
+            //"Week"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Week);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[4], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[4], input.InputData.failedFileName[4]);
+
+            //"Day"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Day);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[5], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[5], input.InputData.failedFileName[5]);
+
+            //Select "电"
+            CarbonUsage.DeSelectCommodity(input.InputData.commodityNames[1]);
+            TimeManager.ShortPause();
+
+            /* hour so waste time, so ignore it
+            //Time range = 2012-1-1 to 2012-2-1
+            EnergyViewToolbar.SetDateRange(ManualTimeRange[1].StartDate, ManualTimeRange[1].EndDate);
+            TimeManager.ShortPause();
+
+            EnergyViewToolbar.View(EnergyViewType.List);
+            JazzMessageBox.LoadingMask.WaitSubMaskLoading();
+            TimeManager.LongPause();
+
+            //"Hour"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Hour);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[6], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[6], input.InputData.failedFileName[6]);
+
+            //Time range = 2012-12-1 to 2012-12-31
+            EnergyViewToolbar.SetDateRange(ManualTimeRange[2].StartDate, ManualTimeRange[2].EndDate);
+            TimeManager.ShortPause();
+
+            EnergyViewToolbar.View(EnergyViewType.List);
+            JazzMessageBox.LoadingMask.WaitSubMaskLoading();
+            TimeManager.LongPause();
+
+            //"Hour"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Hour);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[7], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[7], input.InputData.failedFileName[7]);
+
+            //Time range = 2013-1-1 to 2013-2-1
+            EnergyViewToolbar.SetDateRange(ManualTimeRange[3].StartDate, ManualTimeRange[3].EndDate);
+            TimeManager.ShortPause();
+
+            EnergyViewToolbar.View(EnergyViewType.List);
+            JazzMessageBox.LoadingMask.WaitSubMaskLoading();
+            TimeManager.LongPause();
+
+            //"Hour"
+            CarbonUsage.ClickDisplayStep(DisplayStep.Hour);
+            JazzMessageBox.LoadingMask.WaitChartMaskerLoading();
+            TimeManager.MediumPause();
+
+            CarbonUsage.ExportExpectedDataTableToExcel(input.ExpectedData.expectedFileName[8], DisplayStep.Default);
+            TimeManager.MediumPause();
+            CarbonUsage.CompareDataViewCarbonUsage(input.ExpectedData.expectedFileName[8], input.InputData.failedFileName[8]);
+            */
+        }
+    }
+}

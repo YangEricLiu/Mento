50cb114c02b36e58b24a0fd3958c03afc1e7e1fb
Min YU <min-amy.yu@schneider-electric.com>
2015-01-13 01:54:34 +0000

    modify char limitation for customer code and  common code fields
    git-tfs-id: [http://10.177.1.111:8080/tfs/kara]$/Mento/Trunk;C12831

--- App/Src/Api/Mento.TestApi.TestData/CommonInputData.cs
+++ App/Src/Api/Mento.TestApi.TestData/CommonInputData.cs
@@ -16,7 +16,6 @@ namespace Mento.TestApi.TestData
         public string AccountID { get; set; }
         public string Password { get; set; }
         public string RealName { get; set; }
-        public string CustomerName { get; set; }
         public string CommonName { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
@@ -36,7 +35,6 @@ namespace Mento.TestApi.TestData
         public string AccountID { get; set; }
         public string Password { get; set; }
         public string RealName { get; set; }
-        public string CustomerName { get; set; }
         public string CommonName { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
